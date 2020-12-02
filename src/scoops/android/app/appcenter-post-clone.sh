62a37f1a03be627a04391bec46d2cc1c6af7963a
JH-Dev-Twitch <jhdevtwitch@gmail.com>
2020-12-02 17:24:30 +0000

    went back to apk

--- src/scoops/android/app/appcenter-post-clone.sh
+++ src/scoops/android/app/appcenter-post-clone.sh
@@ -44,21 +44,21 @@ dart run $CONFIG_FILE_PATH
 
 echo "Flutter Configuration Task Completed!!"
 
-echo "Starting Release Bundle Build..."
+echo "Starting Release APK Build..."
 # build APK
 # if you get "Execution failed for task ':app:lintVitalRelease'." error, uncomment next two lines
 # flutter build apk --debug
 # flutter build apk --profile
 
-#flutter build apk --release -t $MAIN_DART_FILE_PATH
+flutter build apk --release -t $MAIN_DART_FILE_PATH --build-number $APPCENTER_BUILD_ID
 
 # if you need build bundle (AAB) in addition to your APK, uncomment line below and last line of this script.
-flutter build appbundle --release --build-number $APPCENTER_BUILD_ID
+#flutter build appbundle --release --build-number $APPCENTER_BUILD_ID
 
 # copy the APK where AppCenter will find it
-#mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
+mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
 
 # copy the AAB where AppCenter will find it
-mkdir -p android/app/build/outputs/bundle/; mv build/app/outputs/bundle/release/app-release.aab $_
+#mkdir -p android/app/build/outputs/bundle/; mv build/app/outputs/bundle/release/app-release.aab $_
 
-echo "Release Apk Build Completed"
+echo "Release APK Build Completed"
