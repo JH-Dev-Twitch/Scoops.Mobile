eaccb54532fefb3f2548dcb6bd7dd03137c75e25
JH-Dev-Twitch <jhdevtwitch@gmail.com>
2020-12-15 01:59:58 +0000

    updated configuration build process, process now responds to a json file, It will now generate a class in a specified location, more tests are needed to test edge cases like null and locations

--- src/scoops/tool/configuration_setup.dart
+++ src/scoops/tool/configuration_setup.dart
@@ -1,13 +1,6 @@
-import 'dart:io';
-import 'dart:convert';
+import 'package:scoops/core/infrastructure/configuration/app_config_job.dart';
 
 Future<void> main() async {
-  Map<String, String> envVars = Platform.environment;
-  final Map<String, String> configuration = {
-    'APP_CENTER_ANDROID_SECRET': envVars["APP_CENTER_ANDROID_SECRET"]
-  };
-  var config = json.encode(configuration);
-
-  File('lib/core/infrastructure/env.dart')
-      .writeAsString('final Map<String, String> env = ${config};');
+  await ConfigurationJob(outputDir: "lib/core/infrastructure/configuration")
+      .buildConfig();
 }
